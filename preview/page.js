38e14efc0277d8824819febe953ff6cacbd21b5b
Jesse Grant <jesse.grant@gmail.com>
2013-01-23 12:36:01 -0500

    Initial commit for Links plugin pages

---
+++ preview/page.js
@@ -0,0 +1,11 @@
+$(function () {
+  var content = JSON.parse(BL.getContentForPreview().content[0].data);
+  var id = BL.getContentForPreview().content[0].id;
+
+  $('.left').html(content.name);
+
+  $('button').click(function (e) {
+    BL.showContent(id);
+  });
+  BL.previewReady();
+});
---
+++ show/page.js
@@ -0,0 +1,10 @@
+$(function () {
+  var content = JSON.parse(BL.getContentItem());
+  var url = content.url;
+
+  if (url.indexOf('http://') == -1 && url.indexOf('https://') == -1) {
+    url = 'http://' + url;
+  }
+
+  window.location = url;
+});
